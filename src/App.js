cafed159484ad019ff65bbef8687a0855a4cfc6f
schm00g <hk8igureu@relay.firefox.com>
2022-07-20 16:34:19 +0100

    try catch fetch errors loading state

--- src/App.js
+++ src/App.js
@@ -3,31 +3,36 @@ import React, { useEffect, useState } from 'react';
 function App() {
 	const [expenses, setExpenses] = useState(null);
 	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
 
 	useEffect(() => {
-		fetch(`http://www.mocky.io/v2/5c62e7c33000004a00019b05`)
-			.then((response) => {
+		const getData = async () => {
+			try {
+				const response = await fetch(
+					`http://www.mocky.io/v2/5c62e7c33000004a00019b05`
+				);
 				if (!response.ok) {
 					throw new Error(
 						`This is an HTTP error: The status is ${response.status}`
 					);
 				}
-				return response.json();
-			})
-			.then(({ transactions }) => setExpenses(transactions))
-			.catch((error) => {
-				console.error(error.message);
-			});
+				const { transactions } = await response.json();
+				setExpenses(transactions);
+				setError(null);
+			} catch (err) {
+				setError(err.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+		getData();
 	}, []);
-
-	useEffect(() => {
-		setLoading(false);
-		console.log(expenses);
-	}, [expenses]);
 
 	return (
 		<div>
 			<h1>Expenses App</h1>
+			{loading && <pre>Fetching your data...</pre>}
+			{error && <div>{`Problem fetching data - ${error}`}</div>}
 			{expenses &&
 				!loading &&
 				expenses.map(({ id, date, amount, description }) => (
