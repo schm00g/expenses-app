adb6e2b93b6584581679eafa71a3f640eb3ecf88
schm00g <hk8igureu@relay.firefox.com>
2022-07-23 13:30:56 +0100

    table styled component

---
+++ src/components/ExpensesTable.js
@@ -0,0 +1,51 @@
+/* eslint-disable camelcase */
+import React from 'react';
+import styled from 'styled-components'; 
+
+function ExpensesTable({data}) {
+    const Table = styled.table`
+        font-family: monospace;
+        border-collapse: collapse;
+        text-align: left;
+        width: 50%;
+        margin: auto;
+    `;
+    const TableData = styled.td`
+        padding: 1% 0;
+    `;
+    const TableRow = styled.tr`
+    &:nth-child(odd) {
+        background-color: #f4f4f4;
+      }
+    `;
+    const Tag = styled.span`
+      background-color: #e4e4ff;
+      border-radius: 10px;
+      padding: 3px 5px;
+    `;
+    return (
+        <Table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Amount</th>
+                    <th>Description</th>
+                    <th>Category</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map(({ id, date, amount, description, category_title }) => (
+                    <TableRow key={id}>
+                        <TableData>{date}</TableData>
+                        <TableData>{amount.value} {amount.currency_iso}</TableData>
+                        <TableData>{description}</TableData>
+                        <TableData><Tag>{category_title}</Tag></TableData>
+                    </TableRow>
+                ))}
+            </tbody>
+            <tfoot><tr><td>Your 10 smallest expenses</td></tr></tfoot>
+        </Table>
+    )
+}
+
+export default ExpensesTable
