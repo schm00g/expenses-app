a7c22543823fc39e922bacc6ce0e1e876e05277e
schm00g <hk8igureu@relay.firefox.com>
2022-07-23 13:46:09 +0100

    add eslint a11y plugin

--- src/components/ExpensesTable.js
+++ src/components/ExpensesTable.js
@@ -2,27 +2,28 @@
 import React from 'react';
 import styled from 'styled-components'; 
 
+const Table = styled.table`
+    font-family: monospace;
+    border-collapse: collapse;
+    text-align: left;
+    width: 50%;
+    margin: auto;
+`;
+const TableData = styled.td`
+    padding: 1% 0;
+`;
+const TableRow = styled.tr`
+&:nth-child(odd) {
+    background-color: #f4f4f4;
+  }
+`;
+const Tag = styled.span`
+  background-color: #e4e4ff;
+  border-radius: 10px;
+  padding: 3px 5px;
+`;
+
 function ExpensesTable({data}) {
-    const Table = styled.table`
-        font-family: monospace;
-        border-collapse: collapse;
-        text-align: left;
-        width: 50%;
-        margin: auto;
-    `;
-    const TableData = styled.td`
-        padding: 1% 0;
-    `;
-    const TableRow = styled.tr`
-    &:nth-child(odd) {
-        background-color: #f4f4f4;
-      }
-    `;
-    const Tag = styled.span`
-      background-color: #e4e4ff;
-      border-radius: 10px;
-      padding: 3px 5px;
-    `;
     return (
         <Table>
             <thead>
